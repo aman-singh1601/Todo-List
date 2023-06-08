ab7253fec5189a2fae14d24aa9b24a4630ab7d1b
Aman <aman16singh2001@gmail.com>
2023-06-09 00:38:05 +0530

    done with css and html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import logo from './logo.svg';
+import todo_img from './images/todo_image.svg'
+import add_btn_img from './images/add_btn.svg'
+import delete_btn_img from './images/delete_btn.svg'
+
 import './App.css';
+import './components/myStyles.css'
+
 
 function App() {
   return (
-    <div className="App">
+  <div className='main-div'>
+    <div className='child-div'>
+         <figure>
+            <img src={todo_img} alt="*****"/> 
+            <figcaption >Add Your List Here</figcaption> 
+         </figure>
+     
+       <div className="addItems">
+         <div className="input-area">
+            <input placeholder="Add Items..."/>
+            <img src={add_btn_img} title='Add Item' />
+         </div>
+       </div> 
+       <div className='showItems'>
+         <div className="singleItem">
+            <h3>Apple</h3>
+            <img src={delete_btn_img} title='Delete Item'/>
+         </div>
+
+       </div>
+       <div className="remove-btn-continer">
+        <button className="remove-btn"><span>CHECK LIST</span></button>
+       </div>
     </div>
+   </div>
   );
 }
 
---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './myStyles.css'
+
+function Todo(){
+
+    return (
+        <>
+        <div className='main-div'>
+            Hello
+            <div>
+                <span> </span>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Todo
